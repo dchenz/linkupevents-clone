d1cf59d7c462a7f12f8b00bc9c9befa29ae3cb68
chendann <51312422+chendann@users.noreply.github.com>
2022-05-26 16:07:37 +0000

    Remove unnecessary dependencies and use date-fns

--- src/components/BigCalendar.jsx
+++ src/components/BigCalendar.jsx
@@ -1,9 +1,21 @@
-import moment from "moment";
 import React from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import enUS from "date-fns/locale/en-US";
 
-const localizer = momentLocalizer(moment);
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales: {
+    "en-US": enUS,
+  },
+});
 
 export default function BigCalendar({ styleClass, ...props }) {
   return (
