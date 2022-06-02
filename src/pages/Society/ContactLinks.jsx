91f166b0bd8056b95d5d93615554d245bc717c00
Danny <51312422+dchenz@users.noreply.github.com>
2022-06-02 21:56:37 +1000

    Don't show Contacts section if there are none

--- src/pages/Society/ContactLinks.jsx
+++ src/pages/Society/ContactLinks.jsx
@@ -3,27 +3,39 @@ import React from "react";
 import SocialsButton from "./SocialsButton";
 
 export default function ContactLinks({ society }) {
+  const { email, ...socials } = society.socials;
+  const hasSocials = Object.keys(socials).length > 0;
+  // Return NULL if no email nor social media links
+  if (!email && !hasSocials) {
+    return null;
+  }
   return (
     <Box component={Paper} p={3} mb={3}>
       <Typography variant="h5">
         Contact
       </Typography>
-      <Box my={2}>
-        <Typography>
-          Email: {society.socials.email}
-        </Typography>
-      </Box>
-      <Box mt={2}>
-        {
-          Object.entries(society.socials).map((item, k) =>
-            <SocialsButton
-              key={k}
-              type={item[0]}
-              value={item[1]}
-            />
-          )
-        }
-      </Box>
+      {
+        email ?
+          <Box my={2}>
+            <Typography>
+              Email: {email}
+            </Typography>
+          </Box> : null
+      }
+      {
+        hasSocials ?
+          <Box mt={2}>
+            {
+              Object.entries(socials).map((item, k) =>
+                <SocialsButton
+                  key={k}
+                  type={item[0]}
+                  value={item[1]}
+                />
+              )
+            }
+          </Box> : null
+      }
     </Box>
   );
 }
