1c25ebba6593b90b7be82e39aab620259dc6a5bf
Danny <51312422+dchenz@users.noreply.github.com>
2022-06-02 16:36:43 +1000

    Reduce heading sizes more

--- src/theme/index.js
+++ src/theme/index.js
@@ -7,20 +7,20 @@ const getHeadingStyles = ({ breakpoints, ...styles }, baseSize) => ({
   ...styles,
   fontSize: `${baseSize}rem`,
   [theme.breakpoints.down("md")]: {
-    fontSize: `${baseSize - 0.5}rem`
+    fontSize: `${baseSize - 0.2}rem`
   }
 });
 
-theme.typography.h1 = getHeadingStyles(theme.typography.h1, 3);
+theme.typography.h1 = getHeadingStyles(theme.typography.h1, 2.7);
 
-theme.typography.h2 = getHeadingStyles(theme.typography.h2, 2.6);
+theme.typography.h2 = getHeadingStyles(theme.typography.h2, 2.3);
 
-theme.typography.h3 = getHeadingStyles(theme.typography.h3, 2.4);
+theme.typography.h3 = getHeadingStyles(theme.typography.h3, 2.1);
 
-theme.typography.h4 = getHeadingStyles(theme.typography.h4, 2.2);
+theme.typography.h4 = getHeadingStyles(theme.typography.h4, 1.7);
 
-theme.typography.h5 = getHeadingStyles(theme.typography.h5, 2);
+theme.typography.h5 = getHeadingStyles(theme.typography.h5, 1.5);
 
-theme.typography.h6 = getHeadingStyles(theme.typography.h6, 1.8);
+theme.typography.h6 = getHeadingStyles(theme.typography.h6, 1.3);
 
 export default theme;
