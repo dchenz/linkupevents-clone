53d1f359a24522e8e9b9facb0358f68a3322faa8
Danny <51312422+dchenz@users.noreply.github.com>
2022-06-04 01:16:05 +1000

    Fix key error in membership prices

--- src/pages/Society/MembershipJoin.jsx
+++ src/pages/Society/MembershipJoin.jsx
@@ -2,31 +2,24 @@ import { Box, Button, Paper, Typography } from "@mui/material";
 import React from "react";
 import SparcLogoIcon from "./SparcLogoIcon";
 
+const membershipTypes = [
+  ["arc", "Arc Member"],
+  ["non-arc", "Non-Arc Member"],
+  ["associate", "Associate Member"]
+];
+
 export default function MembershipJoin({ society }) {
-  const isFree = society.fees ?
-    Object.values(society.fees).every((cost) => cost == 0) : true;
   return (
     <Box component={Paper} p={3} mb={3}>
       <Typography variant="h5">
         Membership
       </Typography>
-      <Box mt={2}>
-        {
-          isFree ?
-            <Typography>FREE</Typography> :
-            <React.Fragment>
-              <Typography>
-                Arc Member: ${society.fees["arc"]}
-              </Typography>
-              <Typography>
-                Non-Arc Member: ${society.fees["nn-arc"]}
-              </Typography>
-              <Typography>
-                Associate Member: ${society.fees["associate"]}
-              </Typography>
-            </React.Fragment>
-        }
-      </Box>
+      {
+        society.fees ?
+          <Box mt={2}>
+            <MembershipPrice fees={society.fees} />
+          </Box> : null
+      }
       <Box mt={2}>
         <Button
           variant="contained"
@@ -40,5 +33,26 @@ export default function MembershipJoin({ society }) {
         </Button>
       </Box>
     </Box>
+  );
+}
+
+function MembershipPrice({ fees }) {
+  const prices = [];
+  for (const [key, name] of membershipTypes) {
+    if (fees[key]) {
+      prices.push(
+        <Typography key={key}>
+          {name}: ${fees[key]}
+        </Typography>
+      );
+    }
+  }
+  if (prices.length == 0) {
+    return <Typography>FREE</Typography>;
+  }
+  return (
+    <React.Fragment>
+      {prices}
+    </React.Fragment>
   );
 }
