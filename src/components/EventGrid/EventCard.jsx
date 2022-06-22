1fbe55d3fc8649b63bc04576f7b12c8ba4011aa1
Danny <51312422+dchenz@users.noreply.github.com>
2022-06-22 23:12:01 +1000

    Docs for event cards

--- src/components/EventGrid/EventCard.jsx
+++ src/components/EventGrid/EventCard.jsx
@@ -4,15 +4,46 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { Properties } from "../../configuration/properties";
 import { PageRoutes } from "../../configuration/routes";
-import EventTime from "../../pages/Events/EventTime";
 import AvatarLink from "../AvatarLink";
 import IconButton from "../IconButton";
+import EventTime from "./EventTime";
 import "./styles.css";
 
+/**
+ * This card component is used to display an event.
+ *
+ * 1) Top image is the event banner photo and links to LinkUp's page for the event.
+ * 2) Top line of text is the event time (see EventTime.jsx).
+ * 3) Bottom line of text is the event title.
+ * 4) Avatar icons will display host images and link to their Facebook pages. Note that hosts without images are not shown here.
+ * 5) Facebook icon is added for direct access to event pages on Facebook.
+ *
+ * @param {{
+ *  event: {
+ *      hosts: {
+ *        name: string,
+ *        url: string,
+ *        image: string
+ *      }[],
+ *      id: string,
+ *      image_url: string,
+ *      title: string,
+ *      time_start: Date,
+ *      time_finish: Date,
+ *      title: string,
+ *      url: string,
+ *      ...other
+ * } }} props
+ */
 export default function EventCard({ event }) {
   const ref = useRef();
   const [height, setHeight] = useState("auto");
 
+  /*
+    Event banner images come in so many different dimensions.
+    The image will be set to a specific height on render to ensure consistency across
+    event cards being displayed together. This method also preserves aspect ratio.
+  */
   useEffect(() => {
     if (ref?.current) {
       // Must be same ratio as 1920x1080
@@ -20,6 +51,7 @@ export default function EventCard({ event }) {
     }
   }, [ref]);
 
+  // Don't render host avatars if they don't have images (has default image)
   const displayedHosts = useMemo(() => {
     return event.hosts.filter((host) =>
       host.image != Properties.ignoreHostImage);
@@ -66,7 +98,6 @@ export default function EventCard({ event }) {
     </Card>
   );
 }
-
 
 
 
